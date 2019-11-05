dbb1aed02184597a70e5731fab9e58a8a3f4aa37
niedziol <michalekniedziolka@gmail.com>
2019-11-05 18:15:34 +0100

    Add first authentication and login template

--- peaky_twitter/peaky_twitter/urls.py
+++ peaky_twitter/peaky_twitter/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('tweets/', include('tweets.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view()),
 ]
 
 if settings.DEBUG:
