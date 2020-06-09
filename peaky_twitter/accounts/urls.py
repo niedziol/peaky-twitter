6a17944d7785474fca80c5b1c5157ebaee779e13
niedziol <michalekniedziolka@gmail.com>
2020-06-09 18:06:44 +0200

    Add ajax for checking username uniqness.

--- peaky_twitter/accounts/urls.py
+++ peaky_twitter/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup_view
+from .views import signup_view, validate_username
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', signup_view, name="signup")
+    path('signup/', signup_view, name="signup"),
+    path('validate_username/', validate_username, name='validate_username')
 ]
--- peaky_twitter/accounts/views.py
+++ peaky_twitter/accounts/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
+def validate_username(request):
+    username = request.GET.get('username', None)
+    data = {
+        'is_taken': User.objects.filter(username__iexact=username).exists()
+    }
+    return JsonResponse(data)
 
 def signup_view(request):
     form = UserCreationForm(request.POST)
